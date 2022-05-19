909b0c95b7bac60d25b3d6adb6169143bf15cd16
hellozo0 <ahju0011@gmail.com>
2022-05-19 10:16:57 +0900

    [ADD] Utils&Assets 추가

---
+++ Careerly/Careerly/Screens/Hellozo0/Home/HomeVC.swift
@@ -0,0 +1,34 @@
+//
+//  HomeVC.swift
+//  Careerly
+//
+//  Created by 안현주 on 2022/05/19.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+  // MARK: - Vars & Lets Part
+
+
+  // MARK: - UI Component Part
+
+  
+  // MARK: - Life Cycle Part
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+    }
+    
+  // MARK: - IBAction Part
+
+  
+  // MARK: - Custom Method Part
+
+  
+  // MARK: - @objc Function Part
+
+}
+// MARK: - Extension Part
