a3155043ca51c9f7b8e144fe174c4d86a8c60f27
Sejin Lee <lsj8706@naver.com>
2022-05-19 23:00:12 +0900

    [FEAT]: PopularProfileTVC UI 구현

---
+++ Careerly/Careerly/Screens/Home/TabBar/HomeTab/TableViewComponents/PopularProfileTVC/PopularProfileCVC/PopularProfileCVC.swift
@@ -0,0 +1,20 @@
+//
+//  PopularProfileCVC.swift
+//  Careerly
+//
+//  Created by User on 2022/05/19.
+//
+
+import UIKit
+
+class PopularProfileCVC: UICollectionViewCell {
+    // MARK: - Vars & Lets Part
+    static let identifier = "PopularProfileCVC"
+    
+    
+    // MARK: - Life Cycle Part
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
---
+++ Careerly/Careerly/Screens/Home/TabBar/HomeTab/TableViewComponents/PopularProfileTVC/PopularProfileTVC.swift
@@ -0,0 +1,20 @@
+//
+//  PopularProfileTVC.swift
+//  Careerly
+//
+//  Created by User on 2022/05/19.
+//
+
+import UIKit
+
+class PopularProfileTVC: UITableViewCell {
+    // MARK: - Vars & Lets Part
+    static let identifier = "PopularProfileTVC"
+    
+    
+    // MARK: - Life Cycle Part
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
