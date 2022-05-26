bd0e4e1322f0be165da7ae52f33d90254337b1b3
Sejin Lee <lsj8706@naver.com>
2022-05-27 03:56:20 +0900

    [CHORE] : 그림자 UI 수정

--- Careerly/Careerly/Screens/Home/TabBar/HomeTab/TableViewComponents/PopularProfileTVC/PopularProfileCVC/PopularProfileCVC.swift
+++ Careerly/Careerly/Screens/Home/TabBar/HomeTab/TableViewComponents/PopularProfileTVC/PopularProfileCVC/PopularProfileCVC.swift
@@ -33,13 +33,8 @@ class PopularProfileCVC: UICollectionViewCell {
     }
     
     func addShadow() {
-        layer.cornerRadius = 5.0
-        layer.masksToBounds = false
-
-        layer.shadowRadius = 3.5
-        layer.shadowOpacity = 0.2
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 0, height: 3)
+        layer.cornerRadius = 4.0        
+        layer.applyShadow(color: .black, alpha: 0.25, x: 0, y: 4, blur: 4, spread: 0)
     }
     
     func setData(_ data: HotProfile) {
